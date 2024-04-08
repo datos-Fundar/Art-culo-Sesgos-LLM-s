9ab960623c7b2ac504cf4d065294ccb300f5914e
TuQmano <juanpabloruiznicolini@gmail.com>
2024-04-08 15:44:11 -0300

    ajustes minimos modelos multivariados

--- scripts/multivariado_2.R
+++ scripts/multivariado_2.R
@@ -6,36 +6,39 @@
 #conocim politizacion
 #q10newt ingresos
 
-# GPT
+# Levando dataset de distacias
 
-# lpop_distances <- read.csv('./outputs/lpop_distances.csv')
+lpop_distances <- read.csv('./outputs/lpop_distances.csv')
 
-lmodel <- lm((gpt_distance_norm) ~ q2 + q1tb + ur1new + edr + conocim   , data = lpop_distances)
-summary(lmodel)
-predicted_values <- predict(lmodel)
-residuals <- residuals(lmodel)
+## GPT ----
 
-# Crear un gráfico de residuos vs. valores predichos
+lmodel_gpt <- lm((gpt_distance_norm) ~ q2 + q1tb + ur1new + edr + conocim   , data = lpop_distances)
+summary(lmodel_gpt)
+predicted_values <- predict(lmodel_gpt)
+residuals <- residuals(lmodel_gpt)
+
+### Crear un gráfico de residuos vs. valores predichos
 plot(predicted_values, residuals, xlab = "Valores Predichos", ylab = "Residuos", main = "Gráfico de Residuos vs Valores Predichos GPT")
 abline(h = 0, lty = 2)
 
-# Cohere
-lmodel <- lm((cohere_distance_norm) ~ q2 + q1tb +  ur1new + edr + conocim , data = lpop_distances)
-summary(lmodel)
-predicted_values <- predict(lmodel)
-residuals <- residuals(lmodel)
+## Cohere ----
+lmodel_cohere <- lm((cohere_distance_norm) ~ q2 + q1tb +  ur1new + edr + conocim , data = lpop_distances)
+summary(lmodel_cohere)
+predicted_values <- predict(lmodel_cohere)
+residuals <- residuals(lmodel_cohere)
 
-# Crear un gráfico de residuos vs. valores predichos
+### Crear un gráfico de residuos vs. valores predichos
 plot(predicted_values, residuals, xlab = "Valores Predichos", ylab = "Residuos", main = "Gráfico de Residuos vs Valores Predichos")
 abline(h = 0, lty = 2)
 
-# Bard
+## Bard ----
 
-lmodel <- lm((bard_distance_norm) ~ q2 + q1tb +  ur1new + edr + conocim , data = lpop_distances)
-summary(lmodel)
-predicted_values <- predict(lmodel)
-residuals <- residuals(lmodel)
+lmodel_bard <- lm((bard_distance_norm) ~ q2 + q1tb +  ur1new + edr + conocim , data = lpop_distances)
+summary(lmodel_bard)
+predicted_values <- predict(lmodel_bard)
+residuals <- residuals(lmodel_bard)
 
-# Crear un gráfico de residuos vs. valores predichos
+### Crear un gráfico de residuos vs. valores predichos
 plot(predicted_values, residuals, xlab = "Valores Predichos", ylab = "Residuos", main = "Gráfico de Residuos vs Valores Predichos")
 abline(h = 0, lty = 2)
+
